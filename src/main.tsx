8edfcd470930bd786058b684eb69106b3ea93e59
Bernardo Tomas <bernardotomas.c14@gmail.com>
2024-05-21 11:58:39 -0300

    Arrumei correções do linter

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './redux';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    <Provider store ={store}>
+    <Provider store={ store }>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
+    </Provider>,
   );
